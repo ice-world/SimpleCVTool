2214115c7b7977ee97a24993325c7c2da2b7f625
Xunchi Zhang <ice_world@outlook.com>
2023-06-08 14:09:47 +0800

    🐞 fix(binarize_image): 修复了无图片时进行二值化报错的问题

--- binarize_image.py
+++ binarize_image.py
@@ -4,11 +4,11 @@ import cv2
 
 
 def binarize_image(threshold):
-    if len(Va.img_cv.shape) == 2:
-        messagebox.showerror("Error", "已经是灰度图")
-        return
     if Va.img_cv is None:
         messagebox.showerror("Error", "No image loaded.")
+        return
+    if len(Va.img_cv.shape) == 2:
+        messagebox.showerror("Error", "已经是灰度图")
         return
     gray = cv2.cvtColor(Va.img_cv, cv2.COLOR_BGR2GRAY)
     _, binarized = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
